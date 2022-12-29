73dbb685df9a77ef2da4ab2ba14ab0379429c42e
Maciej Wicher <Maciej.Wicher@gmail.com>
2022-12-29 12:26:26 +0100

    D13 first task passed

--- AdventCode2022/13-pairs/pairs.py
+++ AdventCode2022/13-pairs/pairs.py
@@ -1,23 +1,25 @@
 
 file=open('./input.txt', 'r')
 
-def are_ordered(p1, p2):
-  if not p1: return 1
-  if not p2: return -1
+def are_ordered(p1, p2, index):
+  print("."*index*2, p1, "\n", "."*(index*2-1),p2)
   is_p1_list = type(p1) is list
   is_p2_list = type(p2) is list
   if is_p1_list != is_p2_list:
     if is_p1_list:
-      return(are_ordered(p1,[p2]))
+      return are_ordered(p1,[p2],index+1)
     else:
-      return(are_ordered([p1],p2))
+      return are_ordered([p1],p2,index+1)
   if not is_p1_list:
     if p1 < p2: return 1
     if p1 > p2: return -1
     return 0
-  result = are_ordered(p1.pop(0), p2.pop(0))
+  if not p1 and not p2: return 0
+  if not p1: return 1
+  if not p2: return -1
+  result = are_ordered(p1.pop(0), p2.pop(0),index+1)
   if result == 0:
-    return are_ordered(p1,p2)
+    return are_ordered(p1,p2,index+1)
   return result
 
 pair_index = 0
@@ -31,10 +33,13 @@ while True:
   pair2=eval(file.readline().strip())
   empty=file.readline()
   #print(pair1, pair2)
-  print("Checking pair {}: {} vs {}".format(pair_index, pair1, pair2))
-  if are_ordered(pair1, pair2) == 1:
+  print("Checking pair {}: \n{}\n{}".format(pair_index, pair1, pair2))
+  if are_ordered(pair1, pair2,1) == 1:
     index_sum+=pair_index
     print("...They are ordered. Sum of index is:", index_sum)
+  else:
+    print("...They are not ordered. Sorry...")
+  #input()
 file.close()
 
 print("Sum of ordered indexes: ", index_sum)
